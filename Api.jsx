6210184de2c848c21092fbcbe7cd66f5c2b44a74
SanjanaChidar25 <chidarsanjana523@gmail.com>
2025-03-09 11:07:50 +0530

    Hello

---
+++ Api.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Api = () => {
+  return (
+    <div>
+         const Api="https://api.api-ninjas.com/v1/recipe"
+       
+    console.log(Api);
+    </div>
+  )
+}
+
+export default Api
